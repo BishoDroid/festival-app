f80909cd69c3cc1c122b2872e5046944ea90045b
Sabir <sabirmgd@gmail.com>
2019-04-24 22:20:43 +0800

    fixed some conflicts

--- client/src/app/data.service.ts
+++ client/src/app/data.service.ts
@@ -9,11 +9,16 @@ import {Observable} from "rxjs/index";
 export class DataService {
 
 
+
     constructor(private http: HttpClient) {
 
     }
 
+    sendPreQuestionnair(data: any): Observable<any> {
+        return this.http.post('http://localhost:3001/api/user/pre-quest', data, {headers: {"client-id" : "client-1"}});
+    }
+
     sendPostQuestionnair(data: any): Observable<any> {
-        return this.http.post('http://localhost:3001/api/user/pre-quest', data, {headers: {}});
+        return this.http.post('http://localhost:3001/api/user/post-quest', data, {headers: {"client-id" : "client-1"}});
     }
 }
