8565a17377bafe40a7e9903f2ce4646896c4f452
Sabir <sabirmgd@gmail.com>
2019-05-19 18:47:36 +0800

    added the skeleton for report download

--- client/src/app/components/dashboard/dashboard.component.ts
+++ client/src/app/components/dashboard/dashboard.component.ts
@@ -17,6 +17,8 @@ export class DashboardComponent implements OnInit {
     selectedChart: number;
     ageChart = [];
     feelingsChart = [];
+    kimaSessionsReportArray = [];
+    kimaUsersReportArray = [] ;
 
     constructor(public dataSvc: DataService) {
     }
@@ -24,10 +26,14 @@ export class DashboardComponent implements OnInit {
     ngOnInit() {
 
         this.dataSvc.getAllSessions().subscribe(res => {
+            this.sessions = res.sessions;
+
+            this.formUsersReport(this.sessions);
+            let feelingsDataFromSessions = this.getFeelingsDataDataSet(this.sessions);
 
             let beforeEngagement = {
                 label: 'Before Engagement',
-                data: [3, 4, 5, 6],
+                data: feelingsDataFromSessions[0],
                 backgroundColor: 'rgba(0, 99, 132, 0.6)',
                 borderColor: 'rgba(0, 99, 132, 1)',
                 yAxisID: "y-axis"
@@ -36,7 +42,7 @@ export class DashboardComponent implements OnInit {
             let afterEngagement = {
                 label: 'After Engagement',
                 //data: [3 , 3 , 3 , 3 ],
-                data: [3.7, 8.9, 9.8, 3.7],
+                data: feelingsDataFromSessions[1],
                 backgroundColor: 'rgba(99, 132, 0, 0.6)',
                 borderColor: 'rgba(99, 132, 0, 1)',
                 yAxisID: "y-axis"
@@ -71,7 +77,7 @@ export class DashboardComponent implements OnInit {
             });
 
 
-            this.sessions = res.sessions;
+
             let data = this.getAgeOfParticpantsData(this.sessions);
             console.log(data);
             let ctx = document.getElementById("ageChart");
@@ -99,19 +105,29 @@ export class DashboardComponent implements OnInit {
             console.log(this.sessions);
             this.totalNumberOfSessions = this.sessions.length;
             console.log(this.totalNumberOfSessions);
-            this.sessions.forEach(session => {
+            this.sessions.forEach( (session, index) => {
                 session.particpantsCompletedPreQuest = this.getNumberOfUsersCompletedPreQuestionair(session);
                 session.particpantsCompletedPostQuest = this.getNumberOfUsersCompletedPostQuestionair(session);
                 console.log(session.recordingStopTime);
                 console.log(session.recordingStartTime);
                 let stop = (Date.parse(session.recordingStopTime));
                 let start = (Date.parse(session.recordingStartTime));
-                session.engagementDuration = Math.floor(((stop - start) / 1000) / 60);
+                if (start && stop) {
+                    session.engagementDuration = Math.floor(((stop - start) / 1000) / 60);
+                } else {
+                    session.engagementDuration = 0;
+                }
+
                 if (!isNaN(session.engagementDuration)) {
                     this.totalEngagementTime += session.engagementDuration;
                 }
                 this.totalNumberOfParticipants += session.particpantsCompletedPreQuest;
 
+                this.kimaSessionsReportArray.push("data:text/csv;charset=utf-8," +
+                    "ID/No, Registration Time , Participants Completed Pre Quest ,  Participants Completed Pre Quest , Session duration ");
+
+                let line = (index + 1 ).toString(10) + " ," +  session.timestamp + "," + session.particpantsCompletedPreQuest + "," + session.particpantsCompletedPostQuest + "," + session.engagementDuration  ;
+                this.kimaSessionsReportArray.push(line);
             });
         }, error => {
 
@@ -119,31 +135,62 @@ export class DashboardComponent implements OnInit {
     }
 
     getFeelingsDataDataSet(sessions) {
+        console.log(sessions);
+        let connectionWithOthersBefore = 0;
+        let connectionWithOthersAfter = 0 ;
+        let tuningWithPeopleBefore = 0;
+        let tuningWithPeopleAfter = 0;
+        let lonelinessBefore = 0;
+        let lonelinessAfter = 0;
+        let happinessBefore = 0;
+        let happinessAfter = 0 ;
+        let noOfParticipantsCompletedPreAndPostQuestionaire = 0;
+
+
         // only count users who completed the pre and the post questionnaire
         sessions.forEach(session => {
-            session.forEach(user => {
-
-                if (user.preQuest && user.postQuest) {
-
-
+            session.users.forEach(user => {
+                if (user.preQuest === undefined || user.postQuest === undefined) {
+                    return;
                 }
+                console.log(user);
+                ++noOfParticipantsCompletedPreAndPostQuestionaire;
 
+                connectionWithOthersBefore += user.preQuest.connectionWithOthersScale;
+                connectionWithOthersAfter += user.postQuest.connectionWithOthersScale;
+
+                tuningWithPeopleBefore += user.preQuest.tuningWithPeopleScale;
+                tuningWithPeopleAfter += user.postQuest.tuningWithPeopleScale;
+
+                lonelinessBefore += user.preQuest.lonelinessScale;
+                lonelinessAfter += user.postQuest.lonelinessScale;
+
+                happinessBefore += user.preQuest.happinessScale;
+                happinessAfter += user.postQuest.happinessScale;
             });
+
 
         });
 
+        return ([
+            // tslint:disable-next-line:max-line-length
+            [connectionWithOthersBefore, tuningWithPeopleBefore , lonelinessBefore , happinessBefore].map(x => x  / noOfParticipantsCompletedPreAndPostQuestionaire ),
+            [connectionWithOthersAfter, tuningWithPeopleAfter, lonelinessAfter, happinessAfter].map(x => x  / noOfParticipantsCompletedPreAndPostQuestionaire )
+            ] );
     }
+
 
     getAgeOfParticpantsData(sessions) {
 
         // tslint:disable-next-line:no-unused-expression prefer-const
         let _10to20 = 0, _21to30 = 0, _31to40 = 0, _41to50 = 0, _51to70 = 0;
         let data = [_10to20, _21to30, _31to40, _41to50, _51to70];
-
+        console.log(sessions);
         sessions.forEach(session => {
-
             session.users.forEach(user => {
-
+            if (user.preQuest === undefined) {
+                return;
+            }
                 switch (true) {
                     case (user.preQuest.age >= 10 && user.preQuest.age <= 20 ):
                         data[0] += 1;
@@ -180,6 +227,49 @@ export class DashboardComponent implements OnInit {
         return session.users.filter(user => user.postQuest !== undefined).length;
     }
 
+    formUsersReport(sessions) {
+
+        this.kimaUsersReportArray.push("data:text/csv;charset=utf-8," +
+            "Session no, Age , Gender ,  Pre-Connection , Pre-Tuning , Pre-Loneliness , Pre-Happiness ," +
+            " Post-Connection , Post-Tuning , Post-Loneliness , Post-Happiness ");
+
+        let lineCommon = " " ;
+
+        sessions.forEach(session => {
+            lineCommon += session._id + " , " ;
+
+
+            session.users.forEach(user => {
+                let lineUser = " ";
+
+                if (user.preQuest === undefined) {
+                    lineUser += " , , , , , , ";
+                } else {
+                    lineUser += user.preQuest.age + ", " +
+                                user.preQuest.gender + ", " +
+                                user.preQuest.connectionWithOthersScale + ", " +
+                                user.preQuest.tuningWithPeopleScale + " , " +
+                                user.preQuest.lonelinessScale + " , " +
+                                user.preQuest.happinessScale + " , ";
+                }
+
+                if (user.postQuest === undefined)  {
+                    lineUser += " , , , , ";
+                } else {
+
+                    lineUser += user.postQuest.connectionWithOthersScale + ", " +
+                                user.postQuest.tuningWithPeopleScale + " , " +
+                                user.postQuest.lonelinessScale + " , " +
+                                user.postQuest.happinessScale ;
+                }
+
+                this.kimaUsersReportArray.push(lineCommon + lineUser);
+
+            });
+
+        });
+
+    }
     showGraph(session: any, i: number) {
 
 
@@ -255,20 +345,6 @@ export class DashboardComponent implements OnInit {
                         fill: false
 
                     }
-
-
-
-
-                        /*{
-                         data: fifthValues,
-                         borderColor: "#eecc33",
-                         fill: false
-                         },
-                         {
-                         data: octaveValues,
-                         borderColor: "#ff22ff",
-                         fill: false
-                         }*/
                     ]
                 },
                 options: {
@@ -288,5 +364,39 @@ export class DashboardComponent implements OnInit {
                 }
             });
         }
+    }
+
+    downloadKimaSessions() {
+
+        var csvContent = this.kimaSessionsReportArray.join("\n");
+
+        var encodedUri = encodeURI(csvContent);
+        var link = document.createElement("a");
+        link.setAttribute("href", encodedUri);
+        link.setAttribute("download", "kima_sessions.csv");
+        document.body.appendChild(link); // Required for FF
+
+        link.click(); // This will download the data file named "my_data.csv".
+    }
+
+    downloadKimaUsers() {
+        var csvContent =  this.kimaUsersReportArray.join("\n");
+        var encodedUri = encodeURI(csvContent);
+        var link = document.createElement("a");
+        link.setAttribute("href", encodedUri);
+        link.setAttribute("download", "kima_users.csv");
+        document.body.appendChild(link); // Required for FF
+
+        link.click(); // This will download the data file named "my_data.csv".
+    }
+
+
+    downloadSymbiosisSessions() {
+
+    }
+
+
+    downloadSymbiosisUsers() {
+
     }
 }
